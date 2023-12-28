8b72b8f5dfc246c0172910c131c6af5b997e9c0b
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2023-12-29 00:55:08 +0800

    First commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,12 @@
-require('@nomicfoundation/hardhat-toolbox');
-
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-  paths: {
-    artifacts: "./app/src/artifacts",
-  }
+  solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
